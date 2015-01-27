981a3700a1247b48757a8f751588590ba44403a1
Stefan Bauer <sauberzof@gmail.com>
2015-01-27 11:02:56 +0100

    uikit updated to version 2.16.2

--- site/uikit/js/core/toggle.js
+++ site/uikit/js/core/toggle.js
@@ -1,64 +1,103 @@
-/*! UIkit 2.11.0 | http://www.getuikit.com | (c) 2014 YOOtheme | MIT License */
-
+/*! UIkit 2.16.2 | http://www.getuikit.com | (c) 2014 YOOtheme | MIT License */
 (function(global, $, UI){
 
-    var togglers = [];
+    "use strict";
+
+    var toggles = [];
 
     UI.component('toggle', {
 
         defaults: {
-            target: false,
-            cls: 'uk-hidden'
+            target    : false,
+            cls       : '@-hidden',
+            animation : false,
+            duration  : 200
+        },
+
+        boot: function(){
+
+            // init code
+            UI.ready(function(context) {
+
+                UI.$("[data-@-toggle]", context).each(function() {
+                    var ele = UI.$(this);
+
+                    if (!ele.data("toggle")) {
+                        var obj = UI.toggle(ele, UI.Utils.options(ele.attr("data-@-toggle")));
+                    }
+                });
+
+                setTimeout(function(){
+
+                    toggles.forEach(function(toggle){
+                        toggle.getToggles();
+                    });
+
+                }, 0);
+            });
         },
 
         init: function() {
 
             var $this = this;
 
-            this.getTogglers();
+            this.getToggles();
 
             this.on("click", function(e) {
                 if ($this.element.is('a[href="#"]')) e.preventDefault();
                 $this.toggle();
             });
 
-            togglers.push(this);
+            toggles.push(this);
         },
 
         toggle: function() {
 
             if(!this.totoggle.length) return;
 
-            this.totoggle.toggleClass(this.options.cls);
+            if (this.options.animation && UI.support.animation) {
 
-            if (this.options.cls == 'uk-hidden') {
+                var $this = this, animations = UI.prefix(this.options.animation).split(',');
+
+                if (animations.length == 1) {
+                    animations[1] = animations[0];
+                }
+
+                animations[0] = animations[0].trim();
+                animations[1] = animations[1].trim();
+
+                this.totoggle.css('animation-duration', this.options.duration+'ms');
+
+                if (this.totoggle.hasClass(this.options.cls)) {
+
+                    this.totoggle.toggleClass(this.options.cls);
+
+                    this.totoggle.each(function(){
+                        UI.Utils.animate(this, animations[0]).then(function(){
+                            $(this).css('animation-duration', '');
+                            UI.Utils.checkDisplay(this);
+                        });
+                    });
+
+                } else {
+
+                    this.totoggle.each(function(){
+                        UI.Utils.animate(this, animations[1]+' @-animation-reverse').then(function(){
+                            UI.$(this).toggleClass($this.options.cls).css('animation-duration', '');
+                            UI.Utils.checkDisplay(this);
+                        }.bind(this));
+                    });
+                }
+
+            } else {
+                this.totoggle.toggleClass(this.options.cls);
                 UI.Utils.checkDisplay(this.totoggle);
             }
         },
 
-        getTogglers: function() {
-            this.totoggle = this.options.target ? $(this.options.target):[];
+        getToggles: function() {
+            this.totoggle = this.options.target ? UI.$(this.options.target):[];
         }
     });
 
-    // init code
-    UI.ready(function(context) {
-
-        $("[data-uk-toggle]", context).each(function() {
-            var ele = $(this);
-
-            if (!ele.data("toggle")) {
-               var obj = UI.toggle(ele, UI.Utils.options(ele.attr("data-uk-toggle")));
-            }
-        });
-
-        setTimeout(function(){
-
-            togglers.forEach(function(toggler){
-                toggler.getTogglers();
-            });
-
-        }, 0);
-    });
-
-})(this, jQuery, jQuery.UIkit);
+})(this, jQuery, UIkit);
--- site/uikit/js/core/utility.js
+++ site/uikit/js/core/utility.js
@@ -1,4 +1,4 @@
-/*! UIkit 2.11.0 | http://www.getuikit.com | (c) 2014 YOOtheme | MIT License */
+/*! UIkit 2.16.2 | http://www.getuikit.com | (c) 2014 YOOtheme | MIT License */
 (function($, UI) {
 
     "use strict";
@@ -8,7 +8,23 @@
     UI.component('stackMargin', {
 
         defaults: {
-            'cls': 'uk-margin-small-top'
+            'cls': '@-margin-small-top'
+        },
+
+        boot: function() {
+
+            // init code
+            UI.ready(function(context) {
+
+                UI.$("[data-@-margin]", context).each(function() {
+
+                    var ele = UI.$(this), obj;
+
+                    if (!ele.data("stackMargin")) {
+                        obj = UI.stackMargin(ele, UI.Utils.options(ele.attr("data-@-margin")));
+                    }
+                });
+            });
         },
 
         init: function() {
@@ -30,17 +46,17 @@
                     UI.$win.on("load", fn);
                 });
 
-                return UI.Utils.debounce(fn, 50);
+                return UI.Utils.debounce(fn, 20);
             })());
 
-            UI.$html.on("uk.dom.changed", function(e) {
+            UI.$html.on("changed.uk.dom", function(e) {
                 $this.columns  = $this.element.children();
                 $this.process();
             });
 
-            this.on("uk.check.display", function(e) {
+            this.on("display.uk.check", function(e) {
                 $this.columns = $this.element.children();
-                if(this.element.is(":visible")) this.process();
+                if (this.element.is(":visible")) this.process();
             }.bind(this));
 
             stacks.push(this);
@@ -50,30 +66,7 @@
 
             var $this = this;
 
-            this.revert();
-
-            var skip         = false,
-                firstvisible = this.columns.filter(":visible:first"),
-                offset       = firstvisible.length ? firstvisible.offset().top : false;
-
-            if (offset === false) return;
-
-            this.columns.each(function() {
-
-                var column = $(this);
-
-                if (column.is(":visible")) {
-
-                    if (skip) {
-                        column.addClass($this.options.cls);
-                    } else {
-                        if (column.offset().top != offset) {
-                            column.addClass($this.options.cls);
-                            skip = true;
-                        }
-                    }
-                }
-            });
+            UI.Utils.stackMargin(this.columns, this.options);
 
             return this;
         },
@@ -84,16 +77,81 @@
         }
     });
 
-    // init code
-    UI.ready(function(context) {
+    // responsive iframes
+    UI.ready((function(){
 
-        $("[data-uk-margin]", context).each(function() {
-            var ele = $(this), obj;
+        var iframes = [], check = function() {
 
-            if (!ele.data("stackMargin")) {
-                obj = UI.stackMargin(ele, UI.Utils.options(ele.attr("data-uk-margin")));
+            iframes.forEach(function(iframe){
+
+                if (!iframe.is(':visible')) return;
+
+                var width  = iframe.parent().width(),
+                    iwidth = iframe.data('width'),
+                    ratio  = (width / iwidth),
+                    height = Math.floor(ratio * iframe.data('height'));
+
+                iframe.css({'height': (width < iwidth) ? height : iframe.data('height')});
+            });
+        };
+
+        UI.$win.on('resize', UI.Utils.debounce(check, 15));
+
+        return function(context){
+
+            UI.$('iframe.@-responsive-width', context).each(function(){
+
+                var iframe = $(this);
+
+                if (!iframe.data('responsive') && iframe.attr('width') && iframe.attr('height')) {
+
+                    iframe.data('width'     , iframe.attr('width'));
+                    iframe.data('height'    , iframe.attr('height'));
+                    iframe.data('responsive', true);
+                    iframes.push(iframe);
+                }
+            });
+
+            check();
+        };
+
+    })());
+
+
+    // helper
+
+    UI.Utils.stackMargin = function(elements, options) {
+
+        options = $.extend({
+            'cls': '@-margin-small-top'
+        }, options);
+
+        options.cls = UI.prefix(options.cls);
+
+        elements = $(elements).removeClass(options.cls);
+
+        var skip         = false,
+            firstvisible = elements.filter(":visible:first"),
+            offset       = firstvisible.length ? (firstvisible.position().top + firstvisible.outerHeight()) - 1 : false; // (-1): weird firefox bug when parent container is display:flex
+
+        if (offset === false) return;
+
+        elements.each(function() {
+
+            var column = UI.$(this);
+
+            if (column.is(":visible")) {
+
+                if (skip) {
+                    column.addClass(options.cls);
+                } else {
+
+                    if (column.position().top >= offset) {
+                        skip = column.addClass(options.cls);
+                    }
+                }
             }
         });
-    });
+    };
 
-})(jQuery, jQuery.UIkit);
+})(jQuery, UIkit);
