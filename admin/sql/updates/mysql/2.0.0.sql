eb87b30e44cc6fc21972090fba395d2b7b6c92e4
Stefan Bauer <sauberzof@gmail.com>
2015-01-28 08:56:45 +0100

    SQL-Schema Update
    table genres added

--- admin/sql/updates/mysql/2.0.0.sql
+++ admin/sql/updates/mysql/2.0.0.sql
@@ -2,6 +2,7 @@ ALTER TABLE `#__lupo_game`
   ADD COLUMN `keywords` VARCHAR (255) NULL AFTER `players`
   , ADD COLUMN `genres` VARCHAR (255) NULL AFTER `keywords`;
 
+
 CREATE TABLE `#__lupo_game_documents` (
   `id` int NOT NULL AUTO_INCREMENT,
   `gameid` int(11) NOT NULL,
@@ -11,4 +12,13 @@ CREATE TABLE `#__lupo_game_documents` (
   `value` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `documents_gameid` (`gameid`)
-)ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `#__lupo_genres` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `genre` char(30) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `genre` (`genre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
