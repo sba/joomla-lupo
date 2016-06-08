da212fc6ab16a21ae6a60b75b7a0581720a9f5b7
Stefan Bauer <sauberzof@gmail.com>
2016-06-08 07:19:07 +0200

    added possibility to output toy categories with details

--- site/views/agecategories/tmpl/default.php
+++ site/views/agecategories/tmpl/default.php
@@ -24,6 +24,8 @@ $menu =JSite::getMenu()->getActive();
 			<h2 class="contentheading"><?php echo $menu->params->get('page_heading')?></h2>
 		<?php endif; ?>
 
+		<?php if($componentParams->get('cats_show_table_with_details', 0)=='0'){ ?>
+
 		<ul class="lupo lupo_categories">
 			<?php
 			foreach($this->agecategories as $agecategory){ ?>
@@ -34,6 +36,34 @@ $menu =JSite::getMenu()->getActive();
 				</li>
 			<?php } ?>
 		</ul>
+
+		<?php } else { ?>
+
+		<table class="uk-table uk-table-striped uk-table-condensed" id="lupo_category_table">
+			<?php
+			foreach($this->agecategories as $agecategory){ ?>
+				<tr>
+					<td width="100">
+						<a href="<?php echo $agecategory['link']?>">
+							<?php if($agecategory['image_thumb']!=null) { ?>
+								<img class="uk-align-left" src="<?php echo $agecategory['image_thumb']?>">
+							<?php } else { ?>
+								<img class="uk-align-left" src="images/spiele/mini_dice-gray.jpg">
+							<?php } ?>
+						</a>
+					</td>
+					<td>
+						<a href="<?php echo $agecategory['link']?>"><?php echo $agecategory['title']?></a>
+						<?php if($componentParams->get('cats_nbr_games', '1')) { ?>
+							(<?php echo $agecategory['number']?>)
+						<?php } ?>
+						<p class="uk-margin-remove"><?php echo $agecategory['description']?></p>
+					</td>
+				</tr>
+			<?php } ?>
+		</table>
+
+		<?php } ?>
 
 	</div>
 </article>
--- site/views/categories/tmpl/default.php
+++ site/views/categories/tmpl/default.php
@@ -23,6 +23,8 @@ $menu = JSite::getMenu()->getActive();
 		<?php if ($menu->params->get('show_page_heading', 1)) : ?>
 			<h2 class="contentheading"><?php echo $menu->params->get('page_heading')?></h2>
 		<?php endif; ?>
+		
+		<?php if($componentParams->get('cats_show_table_with_details', 0)=='0'){ ?>
 
 		<ul class="lupo lupo_categories">
 			<?php
@@ -34,6 +36,34 @@ $menu = JSite::getMenu()->getActive();
 				</li>
 			<?php } ?>
 		</ul>
+
+		<?php } else { ?>
+
+		<table class="uk-table uk-table-striped uk-table-condensed" id="lupo_category_table">
+			<?php
+			foreach($this->categories as $category){ ?>
+				<tr>
+					<td width="100">
+						<a href="<?php echo $category['link']?>">
+							<?php if($category['image_thumb']!=null) { ?>
+							<img class="uk-align-left" src="<?php echo $category['image_thumb']?>">
+							<?php } else { ?>
+							<img class="uk-align-left" src="images/spiele/mini_dice-gray.jpg">
+							<?php } ?>
+						</a>
+					</td>
+					<td>
+						<a href="<?php echo $category['link']?>"><?php echo $category['title']?></a>
+						<?php if($componentParams->get('cats_nbr_games', '1')) { ?>
+							(<?php echo $category['number']?>)
+						<?php } ?>
+						<p class="uk-margin-remove"><?php echo $category['description']?></p>
+					</td>
+				</tr>
+			<?php } ?>
+		</table>
+
+		<?php } ?>
 
 	</div>
 </article>
