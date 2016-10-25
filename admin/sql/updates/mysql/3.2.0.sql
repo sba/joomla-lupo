b4c64358a822ff611f0332c34eab8fcce8ed8184
Stefan Bauer <sauberzof@gmail.com>
2016-10-25 10:10:30 +0200

    fix typo in sql-migration

--- admin/sql/updates/mysql/3.2.0.sql
+++ admin/sql/updates/mysql/3.2.0.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `#___lupo_clients_borrowed` ADD COLUMN `tax_extended` FLOAT DEFAULT 0 NULL AFTER `adrnr`;
+ALTER TABLE `#__lupo_clients_borrowed` ADD COLUMN `tax_extended` FLOAT DEFAULT 0 NULL AFTER `adrnr`;
