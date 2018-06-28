4cc162e679dc2e1d5634486af0c5e4933be0b352
Stefan Bauer <sba@gmx.ch>
2018-06-28 14:21:40 +0200

    show admin button 'reprocess xml' to all admin-users

--- admin/views/lupos/tmpl/default.php
+++ admin/views/lupos/tmpl/default.php
@@ -125,17 +125,10 @@ JHtml::_('behavior.tooltip');
 	<br />
 	<br />
 
-    <?php
-    //show processxml button only to me (if user is superadmin)
-    $user = JFactory::getUser();
-    $isroot = $user->authorise('core.admin');
-    if($isroot){ ?>
 	<form action="<?php echo JRoute::_('index.php?option=com_lupo'); ?>" method="post" name="processXMLForm">
 		<input type="hidden" name="act" value="processxml" />
 		<input type="submit" name="submit" class="btn" value="<?php echo JText::_("COM_LUPO_ADMIN_PROCESS_AGAIN")?>" />
 	</form>
-    <?php } ?>
-
 
     <form action="<?php echo JRoute::_('index.php?option=com_lupo'); ?>" method="post" name="deleteImagesForm">
         <input type="hidden" name="act" value="deleteimages" />
