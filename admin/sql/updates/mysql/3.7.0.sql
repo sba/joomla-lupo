958652420ac3862006ccaad113cc2a0c8a947163
Stefan Bauer <sba@gmx.ch>
2018-10-23 07:56:31 +0200

    fix sql fieldname error

--- admin/sql/updates/mysql/3.7.0.sql
+++ admin/sql/updates/mysql/3.7.0.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `#__lupo_clients_borrowed` ADD COLUMN `next-reservation` DATE NULL AFTER `reminder_sent`;
+ALTER TABLE `#__lupo_clients_borrowed` ADD COLUMN `next_reservation` DATE NULL AFTER `reminder_sent`;
