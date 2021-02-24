88a44271ec592fc2230026217e4dbf5b5e1a6c89
Stefan Bauer <sba@gmx.ch>
2021-02-24 16:15:34 +0100

    add filter-buttons / CS

--- site/views/category/tmpl/default.php
+++ site/views/category/tmpl/default.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @package		Joomla
- * @subpackage	LUPO
- * @copyright   Copyright (C) databauer / Stefan Bauer 
- * @author		Stefan Bauer
- * @link		https://www.ludothekprogramm.ch
- * @license		License GNU General Public License version 2 or later
+ * @package        Joomla
+ * @subpackage    LUPO
+ * @copyright   Copyright (C) databauer / Stefan Bauer
+ * @author        Stefan Bauer
+ * @link        https://www.ludothekprogramm.ch
+ * @license        License GNU General Public License version 2 or later
  */
 
 // No direct access to this file
@@ -13,103 +13,171 @@ defined('_JEXEC') or die('Restricted access');
 
 $componentParams = JComponentHelper::getParams('com_lupo');
 
-if($this->foto['show']=='1') {?>
+if ($this->foto['show'] == '1') { ?>
     <style>
         #lupo_category_table tr td > p {
             padding-left: 115px
         }
+
+        .lupo_btn_subset {
+            margin-bottom: 3px;
+        }
+
+        .uk-button-group .uk-button.uk-active {
+            background-color: #d1417e;
+            color: #ffffff;
+            background-image: linear-gradient(to bottom, #ea488c, #c03d74);
+        }
     </style>
+
+    <script type="text/javascript">
+        jQuery(document).ready(function ($) {
+            $('.lupo_btn_subset').click(function(){
+                let filter_age_catids = $(this).data('age_catids');
+                let filter_genres = $(this).data('genres');
+
+                //filter table rows
+                if(filter_age_catids=="*"){
+                    $('#lupo_category_table > tbody > tr').show();
+                } else {
+                    let row_age_catid, row_genres, has_age_catid, has_genres;
+                    $('#lupo_category_table > tbody > tr').each(function () {
+                        row_age_catid = $(this).data('age_catid');
+                        row_genres = $(this).data('genres');
+
+                        has_age_catid = filter_age_catids.includes(row_age_catid);
+                        has_genres = filter_genres.filter(element => row_genres.includes(element));
+
+                        if (has_age_catid || has_genres.length > 0 ) {
+                            $(this).show();
+                        } else {
+                            $(this).hide();
+                        }
+                    })
+                }
+            })
+        })
+    </script>
 <?php } ?>
 
 <article class="tm-article">
     <div class="tm-article-content">
-        <h2 class="contentheading"><?php echo $this->title?></h2>
+        <h2 class="contentheading"><?php echo $this->title ?></h2>
         <br class="clearfix">
-	    <?php echo $this->description?>
+        <?php echo $this->description ?>
         <?php
-        $text = "<b>Lorem ipsum dolor sit amet,</b><br>consectetur adipisicing elit. Animi aspernatur aut deserunt dolorum excepturi, laudantium maxime minus molestiae nihil nobis numquam, quam quasi voluptatibus. Accusamus ipsum nullase isquam eos.";
+        //TODO: fix joomla bug
+        //$text = "<b>Lorem ipsum dolor sit amet,</b><br>consectetur adipisicing elit. Animi aspernatur aut deserunt dolorum excepturi, laudantium maxime minus molestiae nihil nobis numquam, quam quasi voluptatibus. Accusamus ipsum nullase isquam eos.";
         //$text = "<b>zwei DODO sind ein DiDeLiDi</b><br>Neben dem verflixten DoDeLiDo wird jetzt auch DoDo Didelidi gerufen. Wer bei dieser extremen Variante blitzschnell schaltet und die richtige Aussage macht, wird als erster alle seine Karten los.";
-        echo JHtmlString::truncateComplex($text,220,true);
+        //echo JHtmlString::truncateComplex($text,220,true);
+        ?>
+
+        <?php
+        $subsets = json_decode($this->category['subsets'], true);
+        if (is_array($subsets)) {
+            ?>
+            <div class="uk-button-group" data-uk-button-radio>
+                <button class="uk-button lupo_btn_subset" data-age_catids="*" data-genres="*">Alle</button>
+
+                <?php
+                $json_flags = JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE;
+                foreach ($subsets as $subset_desc => $subset) {
+                    $data_age_catids=json_encode($subset['age_catids'], $json_flags);
+                    $data_genres=json_encode($subset['genres'],  $json_flags);
+                    ?>
+                    <button class="uk-button lupo_btn_subset" data-age_catids='<?=$data_age_catids?>' data-genres='<?=$data_genres?>'><?=$subset_desc?></button>
+                    <?php
+                }
+                ?>
+            </div>
+            <?php
+        }
         ?>
 
         <table class="uk-table uk-table-striped uk-table-condensed" id="lupo_category_table">
-            <?php if($componentParams->get('category_show_tableheader', '1')) { ?>
+            <?php if ($componentParams->get('category_show_tableheader', '1')) { ?>
                 <thead>
                 <tr>
                     <th>
                         <?php echo JText::_('COM_LUPO_TOY'); ?>
-                        <?php if($componentParams->get('lupo_show_toystatus', '0')) {?>
-                        <span class="uk-float-right"><?php echo JText::_('COM_LUPO_BORROWING_STATUS'); ?></span>
+                        <?php if ($componentParams->get('lupo_show_toystatus', '0')) { ?>
+                            <span class="uk-float-right"><?php echo JText::_('COM_LUPO_BORROWING_STATUS'); ?></span>
                         <?php } ?>
                     </th>
                     <?php /* if($componentParams->get('category_show_toy_category', '1')) { ?>
                         <th nowrap="nowrap" class="uk-hidden-small"><div align="right"><?php echo JText::_('COM_LUPO_CATEGORY'); ?></div></th>
                     <?php } */ ?>
-                    <?php if($componentParams->get('category_show_toy_age_category', '1')) { ?>
-                        <th nowrap="nowrap" class="uk-hidden-small"><div align="right"><?php echo JText::_('COM_LUPO_AGE_CATEGORY'); ?></div></th>
+                    <?php if ($componentParams->get('category_show_toy_age_category', '1')) { ?>
+                        <th nowrap="nowrap" class="uk-hidden-small">
+                            <div align="right"><?php echo JText::_('COM_LUPO_AGE_CATEGORY'); ?></div>
+                        </th>
                     <?php } ?>
-                    <?php if($componentParams->get('category_show_tax', '1')) { ?>
-                        <th nowrap="nowrap" class="uk-hidden-small"><div align="right"><?php echo JText::_('COM_LUPO_TAX'); ?></div></th>
+                    <?php if ($componentParams->get('category_show_tax', '1')) { ?>
+                        <th nowrap="nowrap" class="uk-hidden-small">
+                            <div align="right"><?php echo JText::_('COM_LUPO_TAX'); ?></div>
+                        </th>
                     <?php } ?>
-                    <?php if($componentParams->get('category_show_toy_nbrdays', '1')) { ?>
-                        <th nowrap="nowrap" class="uk-hidden-small"><div align="right"><?php echo str_replace(" ", "<br>", JText::_('COM_LUPO_DAYS')); ?></div></th>
+                    <?php if ($componentParams->get('category_show_toy_nbrdays', '1')) { ?>
+                        <th nowrap="nowrap" class="uk-hidden-small">
+                            <div align="right"><?php echo str_replace(" ", "<br>", JText::_('COM_LUPO_DAYS')); ?></div>
+                        </th>
                     <?php } ?>
                 </tr>
                 </thead>
             <?php } ?>
             <tbody>
             <?php
-            $i=0;
-            foreach($this->games as $game){
-                if($componentParams->get('lupo_show_toystatus', '0')) {
-                    $availability = '<i class="uk-icon uk-icon-circle '.$game['availability_color'].' uk-float-right availability_dot" title="' . $game['availability_text'] . '"></i>';
+            $i = 0;
+            foreach ($this->games as $game) {
+                if ($componentParams->get('lupo_show_toystatus', '0')) {
+                    $availability = '<i class="uk-icon uk-icon-circle ' . $game['availability_color'] . ' uk-float-right availability_dot" title="' . $game['availability_text'] . '"></i>';
                 } else {
                     $availability = '';
                 }
                 ?>
-                <tr>
+                <tr data-age_catid='<?= $game['age_catid'] ?>' data-genres='<?= json_encode(explode(", ", $game['genres'])) ?>'>
                     <td>
-	                    <?php if($this->foto['show']!='0') {?>
-                            <a class="category" href="<?php echo $game['link']?>"><?php
-			                    if ($game['image_thumb'] != NULL) {
-				                    ?>
-                                    <img class="uk-align-left" src="<?php echo $game['image_thumb']?>">
-			                    <?php } else { ?>
-                                    <?php if($componentParams->get('foto_list_show_placeholder', '1')) { ?>
-                                        <img class="uk-align-left" src="images/spiele/<?php echo $this->foto['prefix']?>dice-gray.jpg">
+                        <?php if ($this->foto['show'] != '0') { ?>
+                            <a class="category" href="<?php echo $game['link'] ?>"><?php
+                                if ($game['image_thumb'] != null) {
+                                    ?>
+                                    <img class="uk-align-left" src="<?php echo $game['image_thumb'] ?>">
+                                <?php } else { ?>
+                                    <?php if ($componentParams->get('foto_list_show_placeholder', '1')) { ?>
+                                        <img class="uk-align-left" src="images/spiele/<?php echo $this->foto['prefix'] ?>dice-gray.jpg">
                                     <?php } ?>
-			                    <?php }?>
+                                <?php } ?>
                             </a>
-	                    <?php }?>
-	                    <?php echo $availability ?>
-                        <?php if($componentParams->get('category_show_detail_link', '1')) {
+                        <?php } ?>
+                        <?php echo $availability ?>
+                        <?php if ($componentParams->get('category_show_detail_link', '1')) {
                             ?>
                             <p>
-                                <a class="category" href="<?php echo $game['link']?>"><?php echo $game['title']?></a>
+                                <a class="category" href="<?php echo $game['link'] ?>"><?php echo $game['title'] ?></a>
                             </p>
                         <?php } else { ?>
-                            <?php echo $game['title']?>
+                            <?php echo $game['title'] ?>
                         <?php } ?>
-                        <?php if($componentParams->get('foto_list_show_description', '1')) { ?>
-                        <p>
-		                    <?php
-		                    $desc = preg_replace("'<(br[^/>]*?/)>'si", ' ', $game['description_full']); //replace <br/> with space
-		                    echo JHtmlString::truncateComplex($desc,220,true);
-		                    ?>
-                        </p>
+                        <?php if ($componentParams->get('foto_list_show_description', '1')) { ?>
+                            <p>
+                                <?php
+                                $desc = preg_replace("'<(br[^/>]*?/)>'si", ' ', $game['description_full']); //replace <br/> with space
+                                echo JHtmlString::truncateComplex($desc, 220, true);
+                                ?>
+                            </p>
                         <?php } ?>
                     </td>
                     <?php /* if($componentParams->get('category_show_toy_category', '1')) { ?>
                         <td align="right" class="uk-hidden-small"><?php echo $game['category']?></td>
                     <?php } */ ?>
-                    <?php if($componentParams->get('category_show_toy_age_category', '1')) { ?>
-                        <td align="right" class="uk-hidden-small"><?php echo $game['age_category']?></td>
+                    <?php if ($componentParams->get('category_show_toy_age_category', '1')) { ?>
+                        <td align="right" class="uk-hidden-small"><?php echo $game['age_category'] ?></td>
                     <?php } ?>
-                    <?php if($componentParams->get('category_show_tax', '1')) { ?>
-                        <td align="right" class="uk-hidden-small"><?php echo number_format($game['tax'],2)?></td>
+                    <?php if ($componentParams->get('category_show_tax', '1')) { ?>
+                        <td align="right" class="uk-hidden-small"><?php echo number_format($game['tax'], 2) ?></td>
                     <?php } ?>
-                    <?php if($componentParams->get('category_show_toy_nbrdays', '1')) { ?>
-                        <td align="right" class="uk-hidden-small"><?php echo $game['days']?></td>
+                    <?php if ($componentParams->get('category_show_toy_nbrdays', '1')) { ?>
+                        <td align="right" class="uk-hidden-small"><?php echo $game['days'] ?></td>
                     <?php } ?>
                 </tr>
                 <?php
