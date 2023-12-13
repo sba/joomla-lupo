a994993c215767e0b77b5d8cecd94c3ee65b24e6
Stefan Bauer <stefan@databauer.ch>
2023-12-13 11:48:03 +0100

    Update SQL_update_template.sql

--- script/SQL_update_template.sql
+++ script/SQL_update_template.sql
@@ -4,23 +4,6 @@ USE `__DATABASE__`;
 #TRUNCATE #__update_sites_extensions;
 
 UPDATE `#__extensions` SET `params` = '{"updatesource":"default","minimum_stability":"4","customurl":"","versioncheck":"1","backupcheck":"1"} ' WHERE `extension_id` = '28'; 
-
 UPDATE `#__users` SET `name` = 'databauer (sba)' WHERE `name` = 'Stefan Bauer (databauer)';
-
-
-#UPDATE `#__extensions` SET enabled=0 WHERE `name`='mod_post_installation_messages';
-#UPDATE `#__extensions` SET enabled=0 WHERE `name`='plg_quickicon_overridecheck';
-#UPDATE `#__extensions` SET enabled=0 WHERE `name`='plg_quickicon_downloadkey';
-#UPDATE `#__extensions` SET enabled=0 WHERE `name`='plg_installer_webinstaller';
-#UPDATE `#__extensions` SET enabled=0 WHERE `name`='plg_system_updatenotification';
-#UPDATE `#__extensions` SET enabled=0 WHERE `name`='plg_system_webauthn';
-#UPDATE `#__extensions` SET enabled=0 WHERE `name`='plg_authentication_ldap';
-#UPDATE `#__extensions` SET enabled=0 WHERE `name`='mod_loginsupport';
-
-#UPDATE `#__extensions` SET enabled=1 WHERE `name`='Inhalt - LUPO Spiel';
-#UPDATE `#__extensions` SET enabled=1 WHERE `name`='plg_system_wf_responsive_widgets';
-#UPDATE `#__extensions` SET enabled=1 WHERE `name`='plg_system_jce';
-
-#UPDATE `#__guidedtours` SET published = 0 WHERE title IN('COM_GUIDEDTOURS_TOUR_GUIDEDTOURS_TITLE','COM_GUIDEDTOURS_TOUR_GUIDEDTOURSTEPS_TITLE','COM_GUIDEDTOURS_TOUR_TAGS_TITLE','COM_GUIDEDTOURS_TOUR_BANNERS_TITLE','COM_GUIDEDTOURS_TOUR_CONTACTS_TITLE','COM_GUIDEDTOURS_TOUR_NEWSFEEDS_TITLE','COM_GUIDEDTOURS_TOUR_SMARTSEARCH_TITLE');
 
 /* ------------------------------------------------------------------------------------------------------- */
