448353e213a963967eb8892edef5d707b4dd8e70
Stefan Bauer <stefan@databauer.ch>
2023-12-19 14:33:20 +0100

    Update SQL_update_template.sql

--- script/SQL_update_template.sql
+++ script/SQL_update_template.sql
@@ -3,7 +3,7 @@ USE `__DATABASE__`;
 #TRUNCATE #__update_sites;
 #TRUNCATE #__update_sites_extensions;
 
-UPDATE `#__extensions` SET `params` = '{"updatesource":"default","minimum_stability":"4","customurl":"","versioncheck":"1","backupcheck":"1"} ' WHERE `extension_id` = '28'; 
-UPDATE `#__users` SET `name` = 'databauer (sba)' WHERE `name` = 'Stefan Bauer (databauer)';
+#UPDATE `#__extensions` SET `params` = '{"updatesource":"default","minimum_stability":"4","customurl":"","versioncheck":"1","backupcheck":"1"} ' WHERE `extension_id` = '28'; 
+UPDATE `#__extensions` SET `enabled` = 0 WHERE `name` = 'plg_quickicon_eos'; 
 
 /* ------------------------------------------------------------------------------------------------------- */
