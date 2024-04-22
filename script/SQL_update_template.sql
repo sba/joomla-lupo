2673788e4ba10ddccc4cf1b26da6dcf60a073aa2
Stefan Bauer <stefan@databauer.ch>
2024-04-22 09:47:39 +0200

    Update SQL_update_template.sql

--- script/SQL_update_template.sql
+++ script/SQL_update_template.sql
@@ -3,8 +3,9 @@ USE `__DATABASE__`;
 TRUNCATE #__update_sites;
 TRUNCATE #__update_sites_extensions;
 
-#UPDATE `#__extensions` SET `params` = '{"updatesource":"default","minimum_stability":"4","customurl":"","versioncheck":"1","backupcheck":"1"} ' WHERE `extension_id` = '28'; 
+INSERT  INTO `#__update_sites`(`update_site_id`,`name`,`type`,`location`,`enabled`,`last_check_timestamp`,`extra_query`,`checked_out`,`checked_out_time`) VALUES (1,'Joomla! Core','collection','https://update.joomla.org/core/list.xml',1,1712815918,'',NULL,NULL);
+INSERT  INTO `#__update_sites_extensions`(`update_site_id`,`extension_id`) VALUES (1,700);
 
-UPDATE `#__extensions` SET enabled=1 WHERE element='lupowidgetkit';
+UPDATE `#__extensions` SET `params` = '{"updatesource":"default","minimum_stability":"4","customurl":"","versioncheck":"1","backupcheck":"1"} ' WHERE `extension_id` = '28'; 
 
 /* ------------------------------------------------------------------------------------------------------- */
