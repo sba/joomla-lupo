50c48f69bc8ef109a92659b1325819fc1afefa01
Stefan Bauer <stefan@databauer.ch>
2024-04-25 14:56:30 +0200

    Update SQL_update_template.sql

--- script/SQL_update_template.sql
+++ script/SQL_update_template.sql
@@ -1,11 +1,12 @@
 USE `__DATABASE__`;
 
-TRUNCATE #__update_sites;
-TRUNCATE #__update_sites_extensions;
+#TRUNCATE #__update_sites;
+#TRUNCATE #__update_sites_extensions;
 
-INSERT  INTO `#__update_sites`(`update_site_id`,`name`,`type`,`location`,`enabled`,`last_check_timestamp`,`extra_query`,`checked_out`,`checked_out_time`) VALUES (1,'Joomla! Core','collection','https://update.joomla.org/core/list.xml',1,1712815918,'',NULL,NULL);
-INSERT  INTO `#__update_sites_extensions`(`update_site_id`,`extension_id`) VALUES (1,700);
+#INSERT  INTO `#__update_sites`(`update_site_id`,`name`,`type`,`location`,`enabled`,`last_check_timestamp`,`extra_query`,`checked_out`,`checked_out_time`) VALUES (1,'Joomla! Core','collection','https://update.joomla.org/core/list.xml',1,1712815918,'',NULL,NULL);
+#INSERT  INTO `#__update_sites_extensions`(`update_site_id`,`extension_id`) VALUES (1,700);
 
-UPDATE `#__extensions` SET `params` = '{"updatesource":"default","minimum_stability":"4","customurl":"","versioncheck":"1","backupcheck":"1"} ' WHERE `extension_id` = '28'; 
+UPDATE `#__extensions` SET `params` = '{"updatesource":"next","minimum_stability":"4","customurl":"","versioncheck":"1","backupcheck":"1"} ' WHERE `extension_id` = '28'; 
+UPDATE `#__updates`SET detailsurl = 'https://update.joomla.org/core/j4/next.xml' WHERE extension_id=700;
 
 /* ------------------------------------------------------------------------------------------------------- */
