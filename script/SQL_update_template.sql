16c764804ba0b65e89e4e400c0c1c910d83ca0d0
Stefan Bauer <stefan@databauer.ch>
2024-06-01 12:15:58 +0200

    Update SQL_update_template.sql

--- script/SQL_update_template.sql
+++ script/SQL_update_template.sql
@@ -3,9 +3,12 @@ USE `__DATABASE__`;
 #TRUNCATE #__update_sites;
 #TRUNCATE #__update_sites_extensions;
 
-UPDATE #__template_styles
-SET params = '{"hue":"hsl(214, 63%, 20%)","bg-light":"#f0f4fb","text-dark":"#495057","text-light":"#ffffff","link-color":"#2a69b8","link-color-dark":"#6fbfdb","special-color":"#001b4c","monochrome":"0","colorScheme":"light","loginLogo":"","loginLogoAlt":"","logoBrandLarge":"","logoBrandLargeAlt":"","logoBrandSmall":"","logoBrandSmallAlt":""}'
-WHERE template = 'atum';
+#UPDATE #__template_styles
+#SET params = '{"hue":"hsl(214, 63%, 20%)","bg-light":"#f0f4fb","text-dark":"#495057","text-light":"#ffffff","link-color":"#2a69b8","link-color-dark":"#6fbfdb","special-color":"#001b4c","monochrome":"0","colorScheme":"light","loginLogo":"","loginLogoAlt":"","logoBrandLarge":"","logoBrandLargeAlt":"","logoBrandSmall":"","logoBrandSmallAlt":""}'
+#WHERE template = 'atum';
+
+UPDATE #__extensions SET params='{"updatesource":"default","minimum_stability":"4","customurl":"","versioncheck":"1","backupcheck":"1"}' WHERE `name` = 'com_joomlaupdate';
+
 
 /* ------------------------------------------------------------------------------------------------------- */
 
