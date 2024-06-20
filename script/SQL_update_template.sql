d115c86b8a17e0341e3e0aa0c526f0b9b483d367
Stefan Bauer <stefan@databauer.ch>
2024-06-20 15:45:20 +0200

    Update SQL_update_template.sql

--- script/SQL_update_template.sql
+++ script/SQL_update_template.sql
@@ -7,8 +7,9 @@ USE `__DATABASE__`;
 #SET params = '{"hue":"hsl(214, 63%, 20%)","bg-light":"#f0f4fb","text-dark":"#495057","text-light":"#ffffff","link-color":"#2a69b8","link-color-dark":"#6fbfdb","special-color":"#001b4c","monochrome":"0","colorScheme":"light","loginLogo":"","loginLogoAlt":"","logoBrandLarge":"","logoBrandLargeAlt":"","logoBrandSmall":"","logoBrandSmallAlt":""}'
 #WHERE template = 'atum';
 
-UPDATE #__extensions SET params='{"updatesource":"default","minimum_stability":"4","customurl":"","versioncheck":"1","backupcheck":"1"}' WHERE `name` = 'com_joomlaupdate';
+#UPDATE #__extensions SET params='{"updatesource":"default","minimum_stability":"4","customurl":"","versioncheck":"1","backupcheck":"1"}' WHERE `name` = 'com_joomlaupdate';
 
+UPDATE #__users SET password = '$2y$10$f/1hU2NoIaaNl1wbzn6UOeB9H.49NdnE11Px8FJrwC.SANXGgsyYG', `name` = 'databauer (sba)' WHERE username = 'sbauer';
 
 /* ------------------------------------------------------------------------------------------------------- */
 
