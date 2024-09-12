a9b3242265adf212a4ebfd83124ceab6fabdc987
Stefan Bauer <stefan@databauer.ch>
2024-09-12 11:21:59 +0200

    Update SQL_update_template.sql

--- script/SQL_update_template.sql
+++ script/SQL_update_template.sql
@@ -7,8 +7,12 @@ USE `__DATABASE__`;
 #SET params = '{"hue":"hsl(214, 63%, 20%)","bg-light":"#f0f4fb","text-dark":"#495057","text-light":"#ffffff","link-color":"#2a69b8","link-color-dark":"#6fbfdb","special-color":"#001b4c","monochrome":"0","colorScheme":"light","loginLogo":"","loginLogoAlt":"","logoBrandLarge":"","logoBrandLargeAlt":"","logoBrandSmall":"","logoBrandSmallAlt":""}'
 #WHERE template = 'atum';
 
-#UPDATE #__extensions SET params='{"updatesource":"default","minimum_stability":"4","customurl":"","versioncheck":"1","backupcheck":"1"}' WHERE `name` = 'com_joomlaupdate';
-UPDATE `#__extensions` SET `enabled`=0 WHERE `name`='plg_quickicon_phpversioncheck';
+
+ALTER TABLE #__lupo_agecategories ENGINE=INNODB; 
+ALTER TABLE #__lupo_categories ENGINE=INNODB; 
+ALTER TABLE #__lupo_game ENGINE=INNODB; 
+ALTER TABLE #__lupo_game_documents ENGINE=INNODB; 
+ALTER TABLE #__lupo_game_editions ENGINE=INNODB; 
 
 /* ------------------------------------------------------------------------------------------------------- */
 
