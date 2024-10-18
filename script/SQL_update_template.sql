10bf5bbd76506dd0b9a93c828ded1374f6c4ab2d
Stefan Bauer <stefan@databauer.ch>
2024-10-18 15:14:56 +0200

    Update SQL_update_template.sql

--- script/SQL_update_template.sql
+++ script/SQL_update_template.sql
@@ -7,12 +7,7 @@ USE `__DATABASE__`;
 #SET params = '{"hue":"hsl(214, 63%, 20%)","bg-light":"#f0f4fb","text-dark":"#495057","text-light":"#ffffff","link-color":"#2a69b8","link-color-dark":"#6fbfdb","special-color":"#001b4c","monochrome":"0","colorScheme":"light","loginLogo":"","loginLogoAlt":"","logoBrandLarge":"","logoBrandLargeAlt":"","logoBrandSmall":"","logoBrandSmallAlt":""}'
 #WHERE template = 'atum';
 
-
-ALTER TABLE #__lupo_agecategories ENGINE=INNODB; 
-ALTER TABLE #__lupo_categories ENGINE=INNODB; 
-ALTER TABLE #__lupo_game ENGINE=INNODB; 
-ALTER TABLE #__lupo_game_documents ENGINE=INNODB; 
-ALTER TABLE #__lupo_game_editions ENGINE=INNODB; 
+UPDATE #__guidedtours SET published = '0' WHERE `uid` = 'joomla-whatsnew-5-2'; 
 
 /* ------------------------------------------------------------------------------------------------------- */
 
