e6d40bb960612b86ebd313cf4183e0762fda2098
Stefan Bauer <stefan@databauer.ch>
2026-03-14 12:22:55 +0100

    Update SQL_update_template.sql

--- script/SQL_update_template.sql
+++ script/SQL_update_template.sql
@@ -3,9 +3,10 @@ USE `__DATABASE__`;
 #TRUNCATE #__update_sites;
 #TRUNCATE #__update_sites_extensions;
 
+TRUNCATE #__session;
 
-UPDATE `#__extensions` SET enabled=0 WHERE folder='editors-xtd' AND element IN('pagebreak','contact','fields','weblink');
-UPDATE `#__extensions` SET enabled=1 WHERE folder='editors-xtd' AND element IN('lupotoy','module');
+#UPDATE `#__extensions` SET enabled=0 WHERE folder='editors-xtd' AND element IN('pagebreak','contact','fields','weblink');
+#UPDATE `#__extensions` SET enabled=1 WHERE folder='editors-xtd' AND element IN('lupotoy','module');
 
 
 /* ------------------------------------------------------------------------------------------------------- */
